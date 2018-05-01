d99977d51d2cb1b2a5ead2ffaf7761c7f456d5de
won <wjin0352@gmail.com>
2018-05-01 15:34:22 -0400

    sample blog app using lodash

---
+++ src/components/posts_index.js
@@ -0,0 +1,38 @@
+import _ from 'lodash';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
+
+class PostsIndex extends Component {
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
+
+  renderPosts() {
+    // we turned this into an object so we can use lodash map to map through objects.
+    return _.map(this.props.posts, post => {
+      return (
+        <li className="list-group-item" key={post.id}>
+          {post.title}
+        </li>
+      );
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Posts</h3>
+        <ul className="list-group">
+          {this.renderPosts()}
+        </ul>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return { posts: state.posts };
+}
+
+export default connect(mapStateToProps, { fetchPosts: fetchPosts })(PostsIndex);
