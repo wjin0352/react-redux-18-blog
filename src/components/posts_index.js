6d3286b5f3f82df139521e9b8a6cc40aa736ed24
won <wjin0352@gmail.com>
2018-05-02 16:09:48 -0400

    adding reduxforms

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,6 +1,7 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { fetchPosts } from '../actions';
 
 class PostsIndex extends Component {
@@ -9,7 +10,7 @@ class PostsIndex extends Component {
   }
 
   renderPosts() {
-    // we turned this into an object so we can use lodash map to map through objects.
+    // we turned this into an object so we can use lodash map function to map through objects.
     return _.map(this.props.posts, post => {
       return (
         <li className="list-group-item" key={post.id}>
@@ -22,6 +23,11 @@ class PostsIndex extends Component {
   render() {
     return (
       <div>
+        <div className="text-xs-right">
+          <Link className="btn btn-primary" to="/posts/new">
+            Add a Post
+          </Link>
+        </div>
         <h3>Posts</h3>
         <ul className="list-group">
           {this.renderPosts()}
